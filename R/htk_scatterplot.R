699207d8b5a78508ef77d82a60df6c8180cb77d3
maxime.Nguyen@oecd.org <Maxime.NGUYEN@oecd.org>
2020-03-04 09:55:19 +0100

    minor changes for more clarity

--- R/htk_scatterplot.R
+++ R/htk_scatterplot.R
@@ -9,7 +9,7 @@ htk_scatterplot <- function(data_source,myvar_x,myvar_y,my_label,Xlabel=NULL,Yla
       geom_text_repel() +
       scale_x_continuous(name="Peak-to-trough of residential investment", limits=c(-100, 0)) +
       scale_y_continuous(name="Time to recovery of real GDP",limits=c(0, 40)) +
-      geom_smooth(method=lm, se = FALSE, color="darkred") +
+      geom_smooth(method=lm, formula = y~x, se = FALSE, color="darkred") +
       labs(title = title,
            subtitle=subtitle,
            x= Xlabel,
@@ -24,3 +24,31 @@ htk_scatterplot <- function(data_source,myvar_x,myvar_y,my_label,Xlabel=NULL,Yla
       theme(plot.subtitle =element_text(size =7, hjust = 0.5))
     return(plot)
     }
+
+
+htk_scatterplot2 <- function(data_source,myvar_x,myvar_y,my_label,Xlabel=NULL,Ylabel=NULL,title=NULL,subtitle=NULL)
+{
+  plot <- ggplot(data =data_source, aes(x=get(myvar_x),y=get(myvar_y),
+                                        label=get(my_label))) +
+                                        #group=get(my_label))) +
+                                        #text=paste0("",get(myvar_x),"\n",
+                                                   # "",round(get(myvar_y),2),"\n"))) +
+    geom_point() +
+    geom_text_repel() +
+    scale_x_continuous(name="Peak-to-trough of residential investment", limits=c(-100, 0)) +
+    scale_y_continuous(name="Time to recovery of real GDP",limits=c(0, 40)) +
+    geom_smooth(method=lm, se = FALSE, aes(x=get(myvar_x), y=get(myvar_y)), color="darkred") +
+    labs(title = title,
+         subtitle=subtitle,
+         x= Xlabel,
+         y = Ylabel) +
+    theme_minimal() + #set background as minimal
+    theme(panel.grid.minor =  element_blank()) + #set background grid as minimal
+    theme(axis.text.x =element_text(size = 11,angle=90)) + #set aesthetic details
+    theme(axis.title.x = element_text(size = 11)) +
+    theme(axis.title.y = element_text(size=11)) +
+    theme(axis.text.y = element_text(size=11)) +
+    theme(plot.title=element_text(face="bold",colour ="steelblue",size=15, hjust =0.5)) +
+    theme(plot.subtitle =element_text(size =7, hjust = 0.5))
+  return(plot)
+}
