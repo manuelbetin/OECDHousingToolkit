2a884b75d0f54502ca02d14582b859390bb5bd4a
Manuel.Betin <manuel.betin@oecd.org>
2020-06-25 11:35:59 +0200

    update text generation with version from Naomie

--- R/htk_text_generator.r
+++ R/htk_text_generator.r
@@ -1,56 +1,55 @@
 
+
 #'@title Create dynamique text from
 #'@description generate dynamic text
+#'@param data dataset with one row per country
 #'@param category name of the category: Efficiency, Affordability, Sustainability
-#'@author Manuel Betin, Federica Depace
+#'@param vars a vector of four variable names that enter the algorithm
+#'@author Manuel Betin, Federica Depace, Naomi Cohen
 #'@return a dataset containing the values of the indicator for each country
 #'and the paragraph of text interpretating the data
 #'@export
 #'
 
-htk_text_generator=function(category,vars,path=NULL){
-  #import outcome variables
-  data("htk_Outcomevars")
-  htk_Outcomevars=htk_Outcomevars
+htk_text_generator=function(data,category,vars){
 
   if(!category %in% c("Efficiency","Sustainability","Inclusiveness")){
     print("Please select a relevant category: Efficicency, Sustainability or Inclusiveness")
     stop()
   }
 
-  if(length(vars)!=3){
+  if(length(vars)!=4){
     print("Please provide three variables")
     stop()
   }
 
-  dt=htk_Outcomevars %>% dplyr::select(country,vars)
-  colnames(dt)=c("country","var1","var2","var3")
+  #select the variables in the database
+  dt=data %>% dplyr::select(country,vars)
+  colnames(dt)=c("country","var1","var2","var3","var4")
 
   #export data
-  rio::export(dt,paste0("htk_paragraphs.csv"),sep=";")
+  # rio::export(dt,paste0("htk_paragraphs.csv"),sep=";")
 
   output <-paste0(category,"_paragraphs.R")
 
-
-  current.folder=system.file("extdata", package = "OECDHousingToolkit")
-  local.folder <- "."
-  file.copy(paste0(current.folder,"/",output), local.folder)
+  # current.folder=system.file("extdata", package = "OECDHousingToolkit")
+  # local.folder <- "."
+  # file.copy(paste0(current.folder,"/",output), local.folder)
 
   source(output)
 
-  #include the paragraphs in the database
-  my_ldcp <- ldcp(data=my_data,glmp=my_glmp,report=my_report)
+  #include the paragraphs in the database for each country
   dt[[category]]=NA
   for(i in 1:length(dt$country)){
     ctry=dt[i,"country"]
-    current_input <- c(round(myvar1[i],2),round(myvar2[i],2),round(myvar3[i],2))
+    current_input <- c(round(myvars[[1]][i],2),round(myvars[[1]][i],2),round(myvars[[1]][i],2),round(myvars[[1]][i],2))
     my_ldcp <- ldcp_run(ldcp=my_ldcp,input=current_input)
     text=gsub("country",ctry,my_ldcp$report$description)
     dt[i,category]=text
   }
   colnames(dt)=c("country",vars,category)
-  file.remove(output)
-  file.remove("htk_paragraphs.csv")
+  #file.remove(output)
+  #file.remove("htk_paragraphs.csv")
   #rio::export(dt,paste0("htk_paragraphs_",category,".csv"),sep=";")
   dt
 }
--- inst/extdata/Efficiency_paragraphs.R
+++ inst/extdata/Efficiency_paragraphs.R
@@ -1,3 +1,12 @@
+#'@title Text generation
+#'@description generate dynamic text using rLDCP algorithm: the file
+#'take as input a dataset with 4 variables and produce 
+#'@author Manuel Betin, Naomi Cohen
+#'@return a dataset containing the values of the indicator for each country
+#'and the paragraph of text interpretating the data
+#'@export a ldcp object containing the text generated
+#'
+
 library(rLDCP)
 
 ####################################################################
@@ -11,27 +20,15 @@ header=TRUE)
 ###################### data definition #############################
 ####################################################################
 
-myvar1 <- c(t(input1[2]))
+#input1=htk_paragraphs
 
- if( min(myvar1) < 0)
-stop("Invalid min value in data structure: NA.")
- if( max(myvar1) > 1)
-stop("Invalid max value in data structure: myvar1.")
+replace_value_na=0 #default value when missing data
 
-myvar2 <- c(t(input1[3]))
-
- if( min(myvar2) < 0)
-stop("Invalid min value in data structure: NA.")
- if( max(myvar2) > 1)
-stop("Invalid max value in data structure: myvar2.")
-
-myvar3 <- c(t(input1[4]))
-
- if( min(myvar3) < 0)
-stop("Invalid min value in data structure: NA.")
- if( max(myvar3) > 1)
-stop("Invalid max value in data structure: myvar3.")
-rm(input1)
+myvars=lapply(2:dim(input1)[2],function(x){
+  myvar <- c(base::t(input1[x]))
+  myvar=myvar%>%replace_na(replace_value_na)
+  myvar
+})
 
 ####################################################################
 ###################### Data Structure definition ##################
@@ -48,9 +45,10 @@ my_data <- data_structure(input,my_method)
 ####################################################################
 ###################### CP definition ###############################
 ####################################################################
-cp_myvar1 <- cp("cp_myvar1", c("myvar1_lowtail","myvar1_low", "myvar1_medium", "myvar1_high","myvar1_uptail"))
-cp_myvar2 <- cp("cp_myvar2", c("myvar2_lowtail","myvar2_low", "myvar2_medium", "myvar2_high","myvar2_uptail"))
-cp_myvar3 <- cp("cp_myvar3", c("myvar3_lowtail","myvar3_low", "myvar3_medium", "myvar3_high","myvar3_uptail"))
+cp_myvar1 <- cp("cp_myvar1", c("cp_error1", "myvar1_lowtail", "myvar1_low", "myvar1_slow", "myvar1_medium", "myvar1_shigh", "myvar1_high","myvar1_uptail"))
+cp_myvar2 <- cp("cp_myvar2", c("cp_error2", "myvar2_lowtail", "myvar2_low", "myvar2_slow", "myvar2_medium", "myvar2_shigh", "myvar2_high","myvar2_uptail"))
+cp_myvar3 <- cp("cp_myvar3", c("cp_error3", "myvar3_lowtail", "myvar3_low", "myvar3_slow", "myvar3_medium", "myvar3_shigh", "myvar3_high","myvar3_uptail"))
+cp_myvar4 <- cp("cp_myvar4", c("cp_error4", "myvar4_verybad", "myvar4_bad", "myvar4_low", "myvar4_good", "myvar4_verygood"))
 cp_profile <- cp("cp_Efficiency", c("Homogeneous","Heterogeneous"))
 cp_Efficiency <- cp("cp_Efficiency", c("all_low","avg","all_high"))
 
@@ -58,145 +56,195 @@ cp_Efficiency <- cp("cp_Efficiency", c("all_low","avg","all_high"))
 ###################### PMs definition ##############################
 ####################################################################
 
-g_pm_myvar1<- function(u,y){
+## myvar1
+g_pm_myvar1 <- function(u,y){
  y$w <- degree_mf(fuzzy_partitions(
-triangle_mf(0, 0.1, 0.15),
-trapezoid_mf(0.15, 0.15, 0.4, 0.45) ,
-triangle_mf(0.4, 0.5, 0.6) ,
-trapezoid_mf(0.55, 0.6, 0.85, 0.85),
-triangle_mf(0.85, 0.95, 1)), u)
+   triangle_mf(0, 0, 0),
+   triangle_mf(0.00001, 0.1, 0.15),
+   trapezoid_mf(0.15, 0.15, 0.35, 0.35) ,
+   trapezoid_mf(0.35, 0.35, 0.4, 0.45),
+   triangle_mf(0.4, 0.5, 0.6) ,
+   trapezoid_mf(0.55, 0.55, 0.6, 0.65),
+   trapezoid_mf(0.65, 0.65, 0.85, 0.85),
+   triangle_mf(0.85, 0.95, 1)), u)
 y }
 
-t_pm_myvar1<- function(y){
+t_pm_myvar1 <- function(y){
 templates <- c(
-" residential investment locate country in the bottom tail of the distribution ",
-" residential investment is below OECD average ",
-" residential investment is close to the OECD average ",
-" residential investment is above OECD average ",
-" residential investment locate country in the upper tail of the distribution "
+" residential investment, is not available for this country",
+" residential investment, locates country in the bottom tail of the distribution",
+" residential investment, is below OECD average",
+" residential investment, is slightly below OECD average",
+" residential investment, is close to the OECD average",
+" residential investment, is slightly above OECD average",
+" residential investment, is above OECD average",
+" residential investment, locates country in the upper tail of the distribution"
 )
 return(templates[which.max(y$w)])
 }
 
-pm_myvar1 <- pm(y=cp_myvar1, g=g_pm_myvar1, t=t_pm_myvar1)
+pm_myvar1 <- rLDCP::pm(y=cp_myvar1, g=g_pm_myvar1, t=t_pm_myvar1)
 
+####################################################################
+
+## myvar2
 g_pm_myvar2<- function(u,y){
  y$w <- degree_mf(fuzzy_partitions(
-   triangle_mf(0, 0.1, 0.15),
-   trapezoid_mf(0.15, 0.15, 0.4, 0.45) ,
+   triangle_mf(0, 0, 0),
+   triangle_mf(0.0001, 0.1, 0.15),
+   trapezoid_mf(0.15, 0.15, 0.35, 0.35) ,
+   trapezoid_mf(0.35, 0.35, 0.4, 0.45),
    triangle_mf(0.4, 0.5, 0.6) ,
-   trapezoid_mf(0.55, 0.6, 0.85, 0.85),
-   triangle_mf(0.85, 0.95, 1)), u)
+   trapezoid_mf(0.55, 0.6, 0.65, 0.65),
+   trapezoid_mf(0.65, 0.65, 0.85, 0.85),
+   triangle_mf(0.85, 0.9, 1)), u)
 y }
 
 t_pm_myvar2<- function(y){
 templates <- c(
-" house price volatility, in the bottom tail of the distribution ",
+" house price volatility, is not available for this country",
+" house price volatility, is in the bottom tail of the distribution",
 " house price volatility, is below the OECD average",
+" house price volatility, is slightly below the OECD average",
 " house price volatility, is close to the OECD average",
+" house price volatility, is slighly above the OECD average",
 " house price volatility, is above the OECD average",
-" house price volatility, in the upper tail of the distribution "
+" house price volatility, is in the upper tail of the distribution"
 )
 return(templates[which.max(y$w)])
 }
 
-pm_myvar2 <- pm(y=cp_myvar2, g=g_pm_myvar2, t=t_pm_myvar2)
+pm_myvar2 <- rLDCP::pm(y=cp_myvar2, g=g_pm_myvar2, t=t_pm_myvar2)
 
+####################################################################
+
+## myvar3
 g_pm_myvar3<- function(u,y){
  y$w <- degree_mf(fuzzy_partitions(
-   triangle_mf(0, 0.1, 0.15),
-   trapezoid_mf(0.15, 0.15, 0.4, 0.45) ,
-   triangle_mf(0.4, 0.5, 0.6) ,
-   trapezoid_mf(0.55, 0.6, 0.85, 0.85),
-   triangle_mf(0.85, 0.95, 1)), u)
-y }
+    triangle_mf(0, 0, 0),
+    triangle_mf(0.0001 , 0.1, 0.15),
+    trapezoid_mf(0.15, 0.15, 0.35, 0.35) ,
+    triangle_mf(0.4, 0.5, 0.6) ,
+    trapezoid_mf(0.55, 0.55, 0.6, 0.65),
+    trapezoid_mf(0.65, 0.65, 0.85, 0.85),
+    triangle_mf(0.85, 0.95, 1)), u)
+ y }
 
 t_pm_myvar3<- function(y){
 templates <- c(
-" Affordability in the bottom tail ",
-" Affordability is below the OECD average",
-" Affordability is close to the OECD average",
-" Affordability is above the OECD average",
-" Affordability  in the upper tail "
+" affordability, is not available for this country",  
+" affordability, is in the bottom tail",
+" affordability, is below the OECD average",
+" affordability, is slighly below the OECD average",
+" affordability, is close to the OECD average",
+" affordability, is slighly above the OECD average",
+" affordability, is above the OECD average",
+" affordability, is in the upper tail"
 )
 return(templates[which.max(y$w)])
 }
 
-pm_myvar3 <- pm(y=cp_myvar3, g=g_pm_myvar3, t=t_pm_myvar3)
+pm_myvar3 <- rLDCP::pm(y=cp_myvar3, g=g_pm_myvar3, t=t_pm_myvar3)
+
+####################################################################
+
+## myvar4
+g_pm_myvar4<- function(u,y){
+  y$w <- degree_mf(fuzzy_partitions(
+    triangle_mf(0.0001 , 0.1, 0.2),
+    triangle_mf(0.2, 0.3, 0.4),
+    triangle_mf(0.4, 0.5, 0.6),
+    triangle_mf(0.6, 0.7, 0.8),
+    triangle_mf(0.8, 0.9, 1)), u)
+ y }
+
+t_pm_myvar4<- function(y){
+  templates <- c(
+    "The data coverage regarding housing for country is very bad: only ",  
+    "The data coverage regarding housing for country is bad: only ",
+    "The data coverage regarding housing for country is low: ",
+    "The data coverage regarding housing for country is good: ",
+    "The data coverage regarding housing for country is very good: "
+  )
+  return(templates[which.max(y$w)])
+}
+
+pm_myvar4 <- rLDCP::pm(y=cp_myvar4, g=g_pm_myvar4, t=t_pm_myvar4)
+
+####################################################################
+
+## overall performance
 
 g_pm_Efficiency<- function(u,y){
 operator <- operator(  min , max )
  y$w <- infer_rules(fuzzy_rules(
 
+fuzzy_rule(0, 1, 1, 1, 0, 0, 0, 0,
+           0, 1, 1, 1, 0, 0, 0, 0,
+           0, 1, 1, 1, 0, 0, 0, 0,
+           0, 1, 0, 0),
+fuzzy_rule(0, 0, 0, 0, 0, 1, 1, 1,
+           0, 0, 0, 0, 0, 1, 1, 1,
+           0, 0, 0, 0, 0, 1, 1, 1,
+           0, 0, 0, 1),
+fuzzy_rule(0, 0, 0, 1, 1, 1 ,0, 0, 
+           0, 0, 0, 1, 1, 1, 0, 0,
+           0, 0, 0, 1, 1, 1, 0, 0,
+           0, 0, 1, 0)
 
-fuzzy_rule(1, 1, 0, 0, 0,
-           1, 1, 0, 0, 0,
-           1, 1, 0, 0, 0,
-           1, 0, 0),
-fuzzy_rule(0, 0, 0, 1, 1,
-           0, 0, 0, 1, 1,
-           0, 0, 0, 1, 1,
-           0, 0, 1),
-fuzzy_rule(0, 1, 1 ,1 ,0,
-           0, 1, 1 ,1 ,0,
-           0, 1, 1 ,1 ,0,
-           0, 1, 0)
 ), operator,
 list( u[[1]]$w, u[[2]]$w, u[[3]]$w ))
 y }
 
 t_pm_Efficiency<- function(y){
 templates <- c(
-"generally speaking its performance are among the bottom OECD performance ",
-"overall its performance are in line with the rest of OECD countries",
-"the efficiency of the housing sector is globally among the top OECD performers"
+" ERROR",  
+" Generally speaking, its performance are among the bottom OECD ones",
+" Overall, its performance are in line with the rest of OECD countries",
+" The efficiency of the housing sector is globally among the top OECD performers"
 )
 return(templates[which.max(y$w)])
 }
 
-pm_Efficiency <- pm(y=cp_Efficiency, g=g_pm_Efficiency, t=t_pm_Efficiency)
+pm_Efficiency <- rLDCP::pm(y=cp_Efficiency, g=g_pm_Efficiency, t=t_pm_Efficiency)
 
+####################################################################
+
+## profile
 g_pm_profile<- function(u,y){
   operator <- operator(  min , max )
   y$w <- infer_rules(fuzzy_rules(
 
-    fuzzy_rule(1, 1, 0, 0, 0,
-               1, 1, 0, 0, 0,
-               1, 1, 0, 0, 0,
+    fuzzy_rule(1, 0, 0, 0, 0, 0, 0, 0,
+               1, 0, 0, 0, 0, 0, 0, 0,
+               1, 0, 0, 0, 0, 0, 0, 0,
                1, 0),
-    fuzzy_rule(0, 0, 0, 1, 1,
-               0, 0, 0, 1, 1,
-               0, 0, 0, 1, 1,
-               1, 0),
-    fuzzy_rule(0, 1, 1 ,1 ,0,
-               0, 1, 1 ,1 ,0,
-               0, 1, 1 ,1 ,0,
-               1, 0 ),
-    fuzzy_rule(0, 1, 1 ,1 ,0,
-               0, 1, 1 ,1 ,0,
-               0, 1, 1 ,1 ,0,
-               1, 0 ),
-    fuzzy_rule(1, 0, 0 ,0 ,1,
-               1, 0, 0 ,0 ,1,
-               1, 0, 0 ,0 ,1,
+    fuzzy_rule(0, 1, 1, 1, 0, 0, 0, 0,
+               0, 1, 1, 1, 0, 0, 0, 0,
+               0, 1, 1, 1, 0, 0, 0, 0,
                0, 1),
-    fuzzy_rule(1, 1, 0 ,1 ,1,
-               1, 1, 0 ,1 ,1,
-               1, 1, 0 ,1 ,1,
+    fuzzy_rule(0, 0, 0, 0, 0, 1, 1, 1,
+               0, 0, 0, 0, 0, 1, 1, 1,
+               0, 0, 0, 0, 0, 1, 1, 1,
+               0, 1),
+    fuzzy_rule(0, 0, 0, 1, 1, 1, 0, 0,
+               0, 0, 0, 1, 1, 1, 0, 0,
+               0, 0, 0, 1, 1, 1, 0, 0,
                0, 1)
+    
   ), operator,
   list( u[[1]]$w, u[[2]]$w, u[[3]]$w ))
   y }
 
 t_pm_profile<- function(y){
   templates <- c(
-    "With respect to efficiency, country displays homegeneous performances among all three dimensions",
-    "With respect to efficiency, country displays homegeneous performances among all three dimensions of efficiency"
+    "With respect to efficiency, country displays heterogeneous performances among all three dimensions",
+    "With respect to efficiency, country displays homogeneous performances among all three dimensions"
   )
   return(templates[which.max(y$w)])
 }
 
-pm_profile <- pm(y=cp_profile, g=g_pm_profile, t=t_pm_profile)
+pm_profile <- rLDCP::pm(y=cp_profile, g=g_pm_profile, t=t_pm_profile)
 
 ####################################################################
 ###################### GLMP definition ##############################
@@ -206,6 +254,7 @@ glmp_method <- function(pm,input){
 pm$pm_myvar1  <- pm_infer(pm$pm_myvar1, input[1])
 pm$pm_myvar2  <- pm_infer(pm$pm_myvar2, input[2])
 pm$pm_myvar3  <- pm_infer(pm$pm_myvar3, input[3])
+pm$pm_myvar4  <- pm_infer(pm$pm_myvar4, input[4])
 pm$pm_Efficiency  <- pm_infer(pm$pm_Efficiency, list(pm$pm_myvar1$y,pm$pm_myvar2$y,pm$pm_myvar3$y))
 pm$pm_profile  <- pm_infer(pm$pm_profile, list(pm$pm_myvar1$y,pm$pm_myvar2$y,pm$pm_myvar3$y))
 pm
@@ -213,36 +262,80 @@ pm
 my_glmp <- glmp(list(pm_myvar1 = pm_myvar1,
                      pm_myvar2 = pm_myvar2,
                      pm_myvar3 = pm_myvar3,
+                     pm_myvar4 = pm_myvar4,
                      pm_Efficiency = pm_Efficiency,
                      pm_profile=pm_profile),
                 glmp_method)
+
 ####################################################################
 ###################### Report Template definition ##################
 ####################################################################
 
 report_method <- function(properties,pm){
-paste (
+  
+  ## specific case for no data at all ##
+  if((pm$pm_myvar1$u == 0.00000) & (pm$pm_myvar2$u == 0.00000) & (pm$pm_myvar3$u == 0.00000)){
+    "There is no data available for this country."
+  } else {
+  
+  ## specific case for extreme values, include ranking of the country ##
+  if(str_detect(pm_report(pm$pm_myvar1), "tail")) {
+  x = rank(myvar1)
+  myperc_var1=paste0(pm_report(pm$pm_myvar1), ". Indeed, country is ranked ", ((length(myvar1)+1)-x[i]), " over ", length(myvar1), " (", round(pm$pm_myvar1$u, digits=3),")")
+  } else {
+    myperc_var1=paste0(pm_report(pm$pm_myvar1)," (", round(pm$pm_myvar1$u, digits=3),")")
+  }
+  
+ if(str_detect(pm_report(pm$pm_myvar2), "tail")) {
+  x = rank(myvar2)
+      if(str_detect(pm_report(pm$pm_myvar1), "tail")) {
+      myperc_var2=paste0(str_split(pm_report(pm$pm_myvar2), "(?<=is)", 2, simplify=TRUE)[1], " also", str_split(pm_report(pm$pm_myvar2), "(?<=is)", 2, simplify=TRUE)[2], ". Indeed, country is ranked ", ((length(myvar2)+1)-x[i]), " over ", length(myvar2), " (", round(pm$pm_myvar2$u, digits=3),")")
+      } else {
+      myperc_var2=paste0(pm_report(pm$pm_myvar2), ". Indeed, country is ranked ", ((length(myvar2)+1)-x[i]), " over ", length(myvar2), " (", round(pm$pm_myvar2$u, digits=3),")")
+  } } 
+  else {
+  myperc_var2=paste0(pm_report(pm$pm_myvar2)," (", round(pm$pm_myvar2$u, digits=3),")")
+  } 
+  
+  if(str_detect(pm_report(pm$pm_myvar3), "tail")) {
+  x = rank(myvar3)
+    if(str_detect(pm_report(pm$pm_myvar2), "tail")) {
+    myperc_var3=paste0(str_split(pm_report(pm$pm_myvar3), "(?<=is)", 2, simplify=TRUE)[1], " also", str_split(pm_report(pm$pm_myvar3), "(?<=is)", 2, simplify=TRUE)[2], ". Indeed, country is ranked ", ((length(myvar3)+1)-x[i]), " over ", length(myvar3), " (", round(pm$pm_myvar3$u, digits=3),")")
+    } else {
+    myperc_var3=paste0(pm_report(pm$pm_myvar3), ". Indeed, country is ranked ", ((length(myvar3)+1)-x[i]), " over ", length(myvar3), " (", round(pm$pm_myvar3$u, digits=3),")")
+  } } 
+  else {
+  myperc_var3=paste0(pm_report(pm$pm_myvar3)," (", round(pm$pm_myvar3$u, digits=3),")")
+  } 
+  
+  paste (
+pm_report(pm$pm_myvar4), round(pm$pm_myvar4$u*100, digits=2), "% of the indicators are available. ",
+
 pm_report(pm$pm_profile),
-",",
-pm_report(pm$pm_Efficiency),
-". The first dimension,",
-pm_report(pm$pm_myvar1)," (", pm$pm_myvar1$u,")",
-". The second indicator of effiency, ",
-pm_report(pm$pm_myvar2), " (", pm$pm_myvar2$u,")",
-". Finally the last dimension,",
-pm_report(pm$pm_myvar3), " (", pm$pm_myvar3$u,")",
- sep="")}
+". ",
+
+ ## no overall performance if heterogeneous ##
+if(str_detect(pm$pm_profile, "homogeneous")) {
+  pm_report(pm$pm_Efficiency)
+} else {
+},
+
+"The first dimension,",
+myperc_var1, 
+". The second one,",
+myperc_var2,
+". Finally, the last indicator of efficiency,",
+myperc_var3,
+".",
+ sep="")
+} }
+
 properties = NULL
 my_report <- report_template(properties,report_method)
+
 ####################################################################
 ###################### LDCP definition #############################
 ####################################################################
 
 my_ldcp <- ldcp(data=my_data,glmp=my_glmp,report=my_report)
-for(i in 1:length(myvar1)){
- current_input <- c(myvar1[i],myvar2[i],myvar3[i])
-my_ldcp <- ldcp_run(ldcp=my_ldcp,input=current_input)
-cat("Input: c(myvar1,myvar2,myvar3), c(", paste(current_input, collapse=",", sep=""), ")", sep="", fill = TRUE)
-cat("Output: ",my_ldcp$report$description,fill = TRUE)
-cat(fill = TRUE)
-}
+
