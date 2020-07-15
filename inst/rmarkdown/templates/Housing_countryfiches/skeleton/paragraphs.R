71a3abc6a6961617560cac9670ad43bede66ea41
Manuel.Betin <manuel.betin@oecd.org>
2020-07-15 15:53:44 +0200

    fixed bug

--- inst/rmarkdown/templates/Housing_countryfiches/skeleton/paragraphs.R
+++ inst/rmarkdown/templates/Housing_countryfiches/skeleton/paragraphs.R
@@ -20,22 +20,14 @@ library(rLDCP)
 ###################### data definition #############################
 ####################################################################
 
-input1=data.frame(dt)
+input1=dt
 
 replace_value_na=0 #default value when missing data
 
 non_num_vars=input1 %>% select_if(is.character)
 #
-colsnum=(2+length(main_vars)):(2*length(main_vars)+1)
-
-#colsnum=2:length(main_vars)
-myvars=lapply(colsnum,function(x){
-  main_vars_direction[x-4]
-  if(3=="decreasing"){
-    myvar <- 1-c(base::t(input1[x]))
-  }else{
-    myvar <- c(base::t(input1[x]))
-  }
+myvars=lapply(2:dim(input1)[2],function(x){
+  myvar <- c(base::t(input1[x]))
   myvar=myvar%>%replace_na(replace_value_na)
   myvar
 })
@@ -45,21 +37,24 @@ rm(input1)
 #provide the labels to the indicators and the categories that will be
 #used in the text as well as definitions for each categories
 
-definition=c(efficiency=" is defined as the capacity of the sector to provide housing that meets demand across the country, to
+definition=c(efficiency="the capacity of the sector to provide housing that meets demand across the country, to
              facilitate residential mobility and to deliver housing that countributes to macroeconomic stability",
-             inclusiveness=" refers to the capacity of the sector to provide affordable homes across tenure modalities,
+             inclusiveness="the capacity of the sector to provide affordable homes across tenure modalities,
              to tackle homelessness and to coordinate housing policies across levels of government",
-             sustainability=" assess the capacity of the sector to built environmentally sustainable housing and to
+             sustainability="the capacity of the sector to built environmentally sustainable housing and to
              design urban areas that support the environment.")
 
 
 mycategory_label=tolower(category)
 mycategory_definition=definition[[mycategory_label]]
 myvar1_label=main_vars_label[1]
+myvar1_sub_label="[Indicator of replacement]"
 
 myvar2_label=main_vars_label[2]
+myvar2_sub_label="[Indicator of replacement]"
 
 myvar3_label=main_vars_label[3]
+myvar3_sub_label="[Indicator of replacement]"
 
 ####################################################################
 ###################### Data Structure definition ##################
@@ -81,6 +76,10 @@ my_data <- data_structure(input,my_method)
 cp_myvar1 <- cp("cp_myvar1", c("nodata","myvar1_lowtail", "myvar1_low", "myvar1_slow", "myvar1_medium", "myvar1_shigh", "myvar1_high","myvar1_uptail"))
 cp_myvar2 <- cp("cp_myvar2", c("nodata","myvar2_lowtail", "myvar2_low", "myvar2_slow", "myvar2_medium", "myvar2_shigh", "myvar2_high","myvar2_uptail"))
 cp_myvar3 <- cp("cp_myvar3", c("nodata","myvar3_lowtail", "myvar3_low", "myvar3_slow", "myvar3_medium", "myvar3_shigh", "myvar3_high","myvar3_uptail"))
+
+cp_myvar1_sub <- cp("cp_myvar1_sub", c("nodata","myvar1_sub_lowtail", "myvar1_sub_low", "myvar1_sub_slow", "myvar1_sub_medium", "myvar1_sub_shigh", "myvar1_sub_high","myvar1_sub_uptail"))
+cp_myvar2_sub <- cp("cp_myvar2_sub", c("nodata","myvar2_sub_lowtail", "myvar2_sub_low", "myvar2_sub_slow", "myvar2_sub_medium", "myvar2_sub_shigh", "myvar2_sub_high","myvar2_sub_uptail"))
+cp_myvar3_sub <- cp("cp_myvar3_sub", c("nodata","myvar3_sub_lowtail", "myvar3_sub_low", "myvar3_sub_slow", "myvar3_sub_medium", "myvar3_sub_shigh", "myvar3_sub_high","myvar3_sub_uptail"))
 
 cp_profile <- cp("cp_profile", c("Homogeneous","Heterogeneous"))
 cp_Efficiency <- cp("cp_Efficiency", c("all_low","avg","all_high"))
@@ -181,6 +180,113 @@ t_pm_myvar3<- function(y){
 pm_myvar3 <- rLDCP::pm(y=cp_myvar3, g=g_pm_myvar3, t=t_pm_myvar3)
 
 #########################################
+## myvar1_sub: replacement value if myvar1 is missing
+
+g_pm_myvar1_sub<- function(u,y){
+  operator <- operator(  min , max )
+  y$w <- infer_rules(fuzzy_rules(
+
+    fuzzy_rule( 0, 1, 1, 1, 1, 1, 1 ,1,
+                1, 1, 1, 1, 1, 1, 1 ,1,
+                1, 0, 0, 0),
+    fuzzy_rule( 1, 0, 0, 0, 0, 0, 0 ,0,
+                0, 0, 0, 0, 0, 1, 1 ,1,
+                0, 0, 0, 1),
+    fuzzy_rule( 1, 0, 0, 0, 0, 0, 0 ,0,
+                0, 0, 0, 1 ,1 ,1 ,0 ,0,
+                0, 0, 1, 0),
+    fuzzy_rule( 1, 0, 0, 0, 0, 0, 0 ,0,
+                0, 1, 1, 1 ,0 ,0 , 0,0,
+                0, 1, 0, 0)
+  ), operator,
+  list( u[[1]]$w, u[[2]]$w))
+  y }
+
+t_pm_myvar1_sub<- function(y){
+  templates <- c(
+    "",
+    paste0(" is missing and replaced by ",myvar1_sub_label," that ranks in the bottom tail"),
+    paste0(" is missing and replaced by ",myvar1_sub_label," that ranks in the average"),
+    paste0(" is missing and replaced by ",myvar1_sub_label," that ranks in the upper tail")
+  )
+  return(templates[which.max(y$w)])
+}
+
+pm_myvar1_sub <- rLDCP::pm(y=cp_myvar1_sub, g=g_pm_myvar1_sub, t=t_pm_myvar1_sub)
+
+#########################################
+## myvar2_sub: replacement value if myvar2 is missing
+
+g_pm_myvar2_sub<- function(u,y){
+  operator <- operator(  min , max )
+  y$w <- infer_rules(fuzzy_rules(
+
+
+    fuzzy_rule( 0, 1, 1, 1, 1, 1, 1 ,1,
+                1, 1, 1, 1, 1, 1, 1 ,1,
+                1, 0, 0, 0),
+    fuzzy_rule( 1, 0, 0, 0, 0, 0, 0 ,0,
+                0, 0, 0, 0, 0, 1, 1 ,1,
+                0, 0, 0, 1),
+    fuzzy_rule( 1, 0, 0, 0, 0, 0, 0 ,0,
+                0, 0, 1, 1 ,1 ,0 ,0 ,0,
+                0, 0, 1, 0),
+    fuzzy_rule( 1, 0, 0, 0, 0, 0, 0 ,0,
+                0, 1, 1, 1 ,0 ,0 , 0,0,
+                0, 1, 0, 0)
+  ), operator,
+  list( u[[1]]$w, u[[2]]$w))
+  y }
+
+t_pm_myvar2_sub<- function(y){
+  templates <- c(
+    "",
+    paste0(" is missing and replaced by ",myvar2_sub_label," that ranks in the bottom tail"),
+    paste0(" is missing and replaced by ",myvar2_sub_label," that ranks in the average"),
+    paste0(" is missing and replaced by ",myvar2_sub_label," that ranks in the upper tail")
+  )
+  return(templates[which.max(y$w)])
+}
+
+pm_myvar2_sub <- rLDCP::pm(y=cp_myvar2_sub, g=g_pm_myvar2_sub, t=t_pm_myvar2_sub)
+
+#########################################
+## myvar1_sub: replacement value if myvar2 is missing
+
+g_pm_myvar3_sub<- function(u,y){
+  operator <- operator(  min , max )
+  y$w <- infer_rules(fuzzy_rules(
+
+
+    fuzzy_rule( 0, 1, 1, 1, 1, 1, 1 ,1,
+                1, 1, 1, 1, 1, 1, 1 ,1,
+                1, 0, 0, 0),
+    fuzzy_rule( 1, 0, 0, 0, 0, 0, 0 ,0,
+                0, 0, 0, 0, 0, 1, 1 ,1,
+                0, 0, 0, 1),
+    fuzzy_rule( 1, 0, 0, 0, 0, 0, 0 ,0,
+                0, 0, 1, 1 ,1 ,0 ,0 ,0,
+                0, 0, 1, 0),
+    fuzzy_rule( 1, 0, 0, 0, 0, 0, 0 ,0,
+                0, 1, 1, 1 ,0 ,0 , 0,0,
+                0, 1, 0, 0)
+  ), operator,
+  list( u[[1]]$w, u[[2]]$w))
+  y }
+
+t_pm_myvar3_sub<- function(y){
+  templates <- c(
+    "",
+    paste0(" is missing and replaced by ",myvar3_sub_label," that ranks in the bottom tail"),
+    paste0(" is missing and replaced by ",myvar3_sub_label," that ranks in the average"),
+    paste0(" is missing and replaced by ",myvar3_sub_label," that ranks in the upper tail")
+  )
+  return(templates[which.max(y$w)])
+}
+
+pm_myvar3_sub <- rLDCP::pm(y=cp_myvar3_sub, g=g_pm_myvar3_sub, t=t_pm_myvar3_sub)
+
+#########################################
 # global performance over the three indicators
 g_pm_Efficiency<- function(u,y){
   operator <- operator(  min , max )
@@ -254,6 +360,11 @@ glmp_method <- function(pm,input){
   pm$pm_myvar1  <- pm_infer(pm$pm_myvar1, input[1])
   pm$pm_myvar2  <- pm_infer(pm$pm_myvar2, input[2])
   pm$pm_myvar3  <- pm_infer(pm$pm_myvar3, input[3])
+
+  pm$pm_myvar1_sub  <- pm_infer(pm$pm_myvar1_sub, list(pm$pm_myvar1$y,pm$pm_myvar2$y)) #update last argument with value of replacement
+  pm$pm_myvar2_sub  <- pm_infer(pm$pm_myvar2_sub, list(pm$pm_myvar2$y,pm$pm_myvar1$y)) #update last argument with value of replacement
+  pm$pm_myvar3_sub  <- pm_infer(pm$pm_myvar3_sub, list(pm$pm_myvar3$y,pm$pm_myvar1$y)) #update last argument with value of replacement
+
   pm$pm_Efficiency  <- pm_infer(pm$pm_Efficiency, list(pm$pm_myvar1$y,pm$pm_myvar2$y,pm$pm_myvar3$y))
   pm$pm_profile  <- pm_infer(pm$pm_profile, list(pm$pm_myvar1$y,pm$pm_myvar2$y,pm$pm_myvar3$y))
   pm
@@ -261,6 +372,9 @@ glmp_method <- function(pm,input){
 my_glmp <- glmp(list(pm_myvar1 = pm_myvar1,
                      pm_myvar2 = pm_myvar2,
                      pm_myvar3 = pm_myvar3,
+                     pm_myvar1_sub = pm_myvar1_sub,
+                     pm_myvar2_sub = pm_myvar2_sub,
+                     pm_myvar3_sub = pm_myvar3_sub,
                      pm_Efficiency = pm_Efficiency,
                      pm_profile = pm_profile),
                 glmp_method)
@@ -283,7 +397,6 @@ report_tail=function(var, nb_indicators){ #error in the function the rank is not
     ctry_x=x[[i]]
     myperc_var=paste0(pm_report(var), ". Indeed, country is ranked ", ((length(myvars[[nb_indicators]])+1)-ctry_x), " over ", length(myvars[[nb_indicators]]), " (", round(var[["u"]], digits=3),") countries")
   } else {
-
     myperc_var=paste0(pm_report(var)," (", round(var[["u"]], digits=3),")")
   }
   return(myperc_var)
@@ -302,15 +415,15 @@ report_method <- function(properties,pm){
     myperc_var3=report_tail(pm$pm_myvar3,3)
 
     paste (
-      str_to_title(mycategory_label),mycategory_definition,".",
+      str_to_title(mycategory_label), " is defined as ",mycategory_definition,".",
       pm_report(pm$pm_profile),
       pm_report(pm$pm_Efficiency),
       "The first dimension,",
-      myperc_var1,
+      myperc_var1,pm_report(pm$pm_myvar1_sub),
       " The second one,",
-      myperc_var2,
+      myperc_var2,pm_report(pm$pm_myvar2_sub),
       paste0(" Finally, the last selected indicator of ",mycategory_label),
-      myperc_var3,
+      myperc_var3,pm_report(pm$pm_myvar3_sub),
       ". Among the factors contributing to those performances we can mention [More from desks]",
       sep="")
   } #close if(no_data)
@@ -324,3 +437,4 @@ my_report <- report_template(properties,report_method)
 ####################################################################
 
 my_ldcp <- ldcp(data=my_data,glmp=my_glmp,report=my_report)
+
