cd0864ac776aed0cc565c85a398cbf620453dce4
Manuel.Betin <manuel.betin@oecd.org>
2020-07-20 13:39:20 +0200

    minor modifs

--- R/cf_lineplot_gen.R
+++ R/cf_lineplot_gen.R
@@ -17,9 +17,11 @@ cf_lineplot_gen <- function(data_source,xvar,yvar,valuevar,title=NULL,
           legend.position ="bottom",
           legend.title=element_blank(),
           legend.text = element_text(size=10),
-          legend.box.margin=margin(t=-30),
-          aspect.ratio=2,
-          legend.margin = margin(0.2, 0.2, 0.2, 0.2, "cm"),
+          legend.justification="center",
+          legend.margin=margin(0,0,0,0),
+          legend.box.margin=margin(0,0,0,0),
+          #aspect.ratio=2,
+          #legend.margin = margin(0.2, 0.2, 0.2, 0.2, "cm"),
           legend.key.size = unit(0.5,"line"),
           axis.title.x = element_text(size = 9),
           axis.title.y = element_text(size=9),
@@ -28,7 +30,6 @@ cf_lineplot_gen <- function(data_source,xvar,yvar,valuevar,title=NULL,
           plot.title=element_text(face="bold",colour="steelblue",size=15, hjust =0.5),
           plot.subtitle =element_text(hjust = 0.5)) +
     geom_line(data=data_source2,aes(x=get(xvar), y=get(valuevar),colour=get(yvar))) +
-    geom_line(data=data_source3,aes(x=get(xvar), y=OECDmean),colour="blue",alpha=0.5) +
-    theme(legend.position = "right")
+    geom_line(data=data_source3,aes(x=get(xvar), y=OECDmean),colour="blue",alpha=0.5)
 
 }
