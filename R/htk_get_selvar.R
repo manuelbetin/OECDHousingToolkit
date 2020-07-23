213925f4c2259f4fae549de1f72b37d03c3dc386
fededp88 <depace.federica@gmail.com>
2020-07-23 09:30:32 +0200

    function to create table

---
+++ R/htk_get_selvar.R
@@ -0,0 +1,29 @@
+htk_get_selvar=function(sel,catalogue,mycategory,type, title=NULL){
+  
+  #' @title Dataset for the description of indicators table
+  #' @descriptionDataset dynamic dataset collecting the vars
+  #' displayed in the fiche
+  #' @param sel the object of the CYC figure
+  if (type=="outcome"){
+    
+    if (length(sel)!=0){
+      sel %>% select(main_v)
+      sel<-c(sel$main_v)
+    }
+    dt_table_1<- catalogue %>% filter(category==mycategory) %>%  
+      select(variable, variable_name, variable_desc) 
+    dt_table_1<-dt_table_1[which(dt_table_1$variable %in% sel), ]
+    
+  }
+  else if (type=="policy"){
+    
+    if (length(sel)!=0){
+      sel<-sel$var_codes
+    }
+    dt_table_1<- catalogue %>% filter(type=="policy") %>%
+      select(variable, variable_name, variable_desc)
+    dt_table_1<-dt_table_1[which(dt_table_1$variable %in% sel), ]
+  }
+ 
+  return(dt_table_1)
+}
