7443de22b3336bf7c7c28f26745d21cfdbd3bcc4
fededp88 <depace.federica@gmail.com>
2020-11-06 19:12:13 +0100

    adjust function for paragraph page 1

--- R/cf_param_paragraph_page_1.R
+++ R/cf_param_paragraph_page_1.R
@@ -28,9 +28,9 @@ cf_param_paragraph_page_1=function(ctry_code,ctry_name,
       my_adv="We do not have data for the country in this dimension"
     }else{
       if (myctry_hh<=ht_avg_OECD-5){
-        my_adv="lower than"
+        my_adv="below"
       } else if (myctry_hh>=ht_avg_OECD+5){
-        my_adv="higher than"
+        my_adv="above"
       } else if (myctry_hh>ht_avg_OECD-5&myctry_hh<ht_avg_OECD+5){
         my_adv="close to"
       }
@@ -46,8 +46,11 @@ cf_param_paragraph_page_1=function(ctry_code,ctry_name,
     if (identical(gr_hp, numeric(0))){
       my_adv="We do not have data for the country in this dimension"
     }else{
-    if (gr_hp$rate<0){
+    if (gr_hp$rate<0 & ctry_code!="DEU"){
       my_sentence=paste0("However, in few countries real house prices remained stable or even decreased, such as in ", ctry_name)
+    }  else if  (gr_hp$rate<0 & ctry_code=="DEU"){
+        my_sentence="In Germany, after declining in the early 2000s, house prices started to rise faster than the OECD average in 2011 and its growth accelerated from 2016
+onwards.The increase in rent prices was moderate compared to OECD countries"
     } else  if (gr_hp$rate>0 & gr_hp$rate<20){
       my_sentence=paste0("However, in few countries real house prices remained stable, such as in ", ctry_name)
     } else if (gr_hp$rate>=20 & gr_hp$rate<=50){
@@ -106,11 +109,11 @@ cf_param_paragraph_page_1=function(ctry_code,ctry_name,
 
   sentence1=paste0("Housing policies affect people’s well-being through a wide range of channels including access to decent shelter, environmental quality, efficient use of scarce resources, type and extent of commuting, as well as its contribution to strong and resilient economic growth. This note provides a cross-country perspective on " ,ctry_name, "’s housing-related indicators and policy settings.")
 
-  sentence2=paste0("Households’ tenure choices depend on demographics and/or socio-economic factors,such as population ageing, as well as policies related to housing taxation and rental regulations. There are large differences in tenure structure across OECD and key partner countries: homeownership in ", ctry_name," is ", sentence_homewonership(ht_avg_OECD, myctry_hh)," OECD average (Figure a). ")
+  sentence2=paste0("Households’ tenure choices depend on demographics and/or socio-economic factors,such as population ageing, as well as policies related to housing taxation and rental regulations. There are large differences in tenure structure across OECD and key partner countries: in terms of homeownership ", ctry_name," is ", sentence_homewonership(ht_avg_OECD, myctry_hh)," OECD average (Figure a). ")
 
-  sentence3=paste0("Real house prices and rent prices have risen strongly in many countries since the 1990s with prices increasing by more than 100% in those countries experiencing the largest increases (Figure b, c). ",sentence_rhp(gr_hp, ctry_code, ctry_name), ".")
+  sentence3=paste0("Real house prices and rent prices have risen strongly in many countries since the 1990s, with prices increasing by more than 100% in those countries experiencing the largest increases (Figure b, c). ",sentence_rhp(gr_hp, ctry_code, ctry_name), ".")
 
-  sentence4=paste0("Finally, mortgage markets play a crucial role in housing markets since housing generally constitutes the household's single largest financial outlay. The share of households' mortgage claims in terms of GDP in ", ctry_name," is ", sentece_mortg(avg_mortg, mortgage_data_myctr,myctry_hh, ht_avg_OECD)," (Figure d).")
+  sentence4=paste0(" Finally, mortgage markets play a crucial role in housing markets since housing generally constitutes the household's single largest financial outlay. The share of outstanding households' mortgage claims in terms of GDP in ", ctry_name," is ", sentece_mortg(avg_mortg, mortgage_data_myctr,myctry_hh, ht_avg_OECD)," (Figure d).")
 
   return(paste0(sentence1,sentence2,sentence3,sentence4, sep=" "))
 
