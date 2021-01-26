edd35e03c0e4812234e38915f5cda9b0957eca5e
fededp88 <depace.federica@gmail.com>
2021-01-26 14:49:46 +0100

    smaller size text in CYC :more

--- R/hkt_cyc_new.R
+++ R/hkt_cyc_new.R
@@ -126,9 +126,9 @@ htk_cyc_new=function(mydata,ranking, ctry,var_codes, sec_col, type_var, title=NU
       geom_point(aes(x=rank_OECD  ) , shape=18, color="darkgreen", size=mysize) +
       geom_point(aes(x=0), shape=1, color='grey', size=mysize)  +
       geom_point(aes(x=1 ), shape=1, color='grey', size=mysize) +
-      geom_text(size=2.8,data=mylabels%>%filter(mycolor=="minmax"),
+      geom_text(size=2.5,data=mylabels%>%filter(mycolor=="minmax"),
                 aes(x=x,vjust=-0.5,label=mylabel,color=mycolor,lineheight = .8))+
-      geom_text_repel(seed = 10, size=2.8,
+      geom_text_repel(seed = 10, size=2.5,
                       data=mylabels%>%filter(mycolor=="OECD"),
                       nudge_y = 0.15,
                       # direction="y",
